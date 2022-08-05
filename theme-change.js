db7506fe147f71044213151eca7f498b7956de22
devenperez <63876261+devenperez@users.noreply.github.com>
2022-08-05 01:06:23 -0400

    Themes written as dictionaries
    This is done for simplicity as the website scales

--- theme-change.js
+++ theme-change.js
@@ -1,7 +1,19 @@
-var themes = {
-    0: ["#404040", "#757575", "#8181C1", "#ffffff", true],
-    1: ["#333333", "#643173", "#7d5ba6", "#ffffff", true]
-};
+const themes = [
+    {
+        "primary-color": "#404040",
+        "secondary-color": "#757575",
+        "accent-color": "#8181C1",
+        "text-color": "#ffffff",
+        "is-inverted": true
+    },
+    {
+        "primary-color": "#333333",
+        "secondary-color": "#643173",
+        "accent-color": "#7d5ba6",
+        "text-color": "#ffffff",
+        "is-inverted": true
+    }
+];
 
 function theme_change() {
     var currentTheme = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--current-theme"));
@@ -9,13 +21,13 @@ function theme_change() {
 
     // Change theme colors
     document.documentElement.style.setProperty("--current-theme", nextThemeIndex);
-    document.documentElement.style.setProperty("--primary-color", themes[nextThemeIndex][0]);
-    document.documentElement.style.setProperty("--secondary-color", themes[nextThemeIndex][1]);
-    document.documentElement.style.setProperty("--accent-color", themes[nextThemeIndex][2]);
-    document.documentElement.style.setProperty("--text-color", themes[nextThemeIndex][3]);
+    document.documentElement.style.setProperty("--primary-color", themes[nextThemeIndex]["primary-color"]);
+    document.documentElement.style.setProperty("--secondary-color", themes[nextThemeIndex]["secondary-color"]);
+    document.documentElement.style.setProperty("--accent-color", themes[nextThemeIndex]["accent-color"]);
+    document.documentElement.style.setProperty("--text-color", themes[nextThemeIndex]["text-color"]);
 
     // Flip black and white images
-    if (themes[currentTheme][4] != themes[nextThemeIndex][4]) {
+    if (themes[currentTheme]["is-inverted"] != themes[nextThemeIndex]["is-inverted"]) {
         let invertables = document.getElementsByClassName("invertable");
         for (let i = 0; i < invertables.length; i++) {
             invertables[i].classList.toggle("inverted");
