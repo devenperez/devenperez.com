ae07d1a919cea05565c106a27616c627c3febd2e
devenperez <63876261+devenperez@users.noreply.github.com>
2022-08-05 22:06:11 -0400

    Leetcode page is now run by JSON file
    This is awesome for me. I love this. This is the most progress I've made in a long time.

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -1,10 +1,46 @@
-async function loadProblems() {
+var problemJson;
+var problemsShown = 0;
+var problemsLoaded = 0;
 
-    const requestURL = 'https://api-git.devenperez.com/data/sample.json';
-    const request = new Request(requestURL);
-  
-    const response = await fetch(request);
-    const solutions = await response.json();
+fetch('https://api-git.devenperez.com/data/sample.json')
+.then((response) => response.json())
+.then((data) =>  {
+    problemJson = data;
+    problemsLoaded = problemJson.solvedProblems.problems.length
+    document.getElementById("solution-timestamp").innerText += ` ${data.timeGathered}`
+    document.getElementById("easy-problem-count").innerText = problemJson.solvedProblems.solvedTotals.easy
+    document.getElementById("medium-problem-count").innerText = problemJson.solvedProblems.solvedTotals.medium
+    document.getElementById("hard-problem-count").innerText = problemJson.solvedProblems.solvedTotals.hard
+    printProblems(1);
+});
 
-    console.log(solutions)
+function printProblems(numToLoad) {
+    for(var i = 0; i < numToLoad; i++) {
+        var prob = problemJson.solvedProblems.problems[problemsShown]
+        problemsShown++;
+        var themeType = (problemsShown % 2 == 0) ? "Light" : "Dark";
+        var oddOrEven = (problemsShown % 2 == 0) ? "even" : "odd";
+        document.getElementById("all-solutions").innerHTML += `<!-- Section ${problemsShown}: ${themeType} Theme --><div class="solution-section ${oddOrEven}-segment"><span class="solution-info"><h1>${prob.number}. ${prob.name} (${prob.difficulty})</h1><br><h3>${prob.scores.time}</h3><h3>${prob.scores.memory}</h3></span><span class="solution-button-panel"><img src="images/code.png" class="invertable inverted"></h2></span></div>`;
+    }
 }
+
+/*
+<!-- Section 1: Dark Theme -->
+<div class="solution-section odd-segment">
+    <span class="solution-info">
+        <h1>1. Some problem name (Easy)</h1>
+        <br>
+        <h3>0.0 ms</h3>
+        <h3>0.0 MB</h3>
+    </span>
+    <span class="solution-button-panel">
+        <img src="images/code.png" class="invertable inverted"></h2>
+    </span>
+</div>
+*/
+
+
+
+/*
+<!-- Section 1: Dark Theme --><div class="solution-section odd-segment"><span class="solution-info"><h1>1. Some problem name (Easy)</h1><br><h3>0.0 ms</h3><h3>0.0 MB</h3></span><span class="solution-button-panel"><img src="images/code.png" class="invertable inverted"></h2></span></div>
+*/
